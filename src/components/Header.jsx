50f1be7940256d4a3a11f58b28cd7c58b8131685
Ella0707 <pakelvi77@gmail.com>
2022-05-10 22:50:19 +0300

    start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
+import s from './Header.module.scss';
 import {RouteNames} from "../router/routeNames";
-import {Link} from "react-router-dom";
-import {Button} from "antd";
+import {Link, NavLink} from "react-router-dom";
+// import {Button} from "antd";
+
+
 
 const Header = () => {
     return (
-        <header>
-            <h1>Header</h1>
-            <Link to={RouteNames.LANDING}>
-               <Button type="primary">back to main page</Button>
-            </Link>
+        <header className={s.header}>
+            <div className={s.header__inner}>
+                <Link className={s.header__logo} to={RouteNames.LANDING}>
+                    {/* <Button type="primary">back to main page</Button> */}
+                    <img src={process.env.PUBLIC_URL + "/img/logo.svg"} alt="" />
+                </Link>
+                <nav className={s.header__nav}>
+                    <ul className={s.header__list}>
+                        <li className={s.header__item}>
+                            <a href="">О проекте</a>
+                        </li>
+                        <li className={s.header__item}>
+                            <a href="">Отзывы</a>
+                        </li>
+                        <li className={s.header__item}>
+                            <a href="">Вопросы и ответы</a>
+                        </li>
+                        <li className={s.header__item}>
+                            <a href="">Контакты</a>
+                        </li>
+                        <li className={s.header__itemUser}>
+                            <div className={s.header__itemImg}>
+                                <img src={process.env.PUBLIC_URL + "/img/user.svg"} alt="" />
+                            </div>
+                            <a href="">Личный кабинет</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={s.header__contact}>
+                    <div className={s.header__tel}>
+                        <a className='tel-link' href="tel:+84951201420">8 (495) 120-14-20</a>
+                        <div className="tel-img">
+                            <img src={process.env.PUBLIC_URL + "/img/tel.svg"} alt="" />
+                        </div>
+                    </div>
+                    <div className={s.header__contactUs}>
+                        <a className="contact-us__link" href="#">
+                            Оставить заявку
+                            <img src={process.env.PUBLIC_URL + "/img/mail.svg"} alt="" />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 };
