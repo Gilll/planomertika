8a6fe7846f578edbc09a25ed6dddab8a0f689566
unknown <pakelvi0707@gmail.com>
2022-06-08 15:25:58 +0300

    work

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -5,12 +5,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import ReviewsItem from "./reviewItem/ReviewsItem";
-
-
-
-
-
-
+import img from './../../asetss/img/rev1.png';
+import img2 from './../../asetss/img/rev1.png';
 
 
 
@@ -21,8 +17,17 @@ const Reviews = () => {
     }
 
     const reviews = [
-        {title: 'title1', comment: 'comment1'},
-        {title: 'title2', comment: 'comment2'}
+        {
+            img: img, name: 'Александр и Валерия Решетниковы',
+            date: 'Москва,  20 мая 2022',
+            text: 'Всем привет!  Начинаются работы на стройке жилого комплекса. Предлагаю здесь обмениваться фотографиями и новостями. Всем привет!  Начинаются работы на стройке жилого комплекса. Всем привет!  Начинаются работы на стройке жилого комплекса. Предлагаю здесь обмениваться фотографиями и новостями. '
+        },
+        {
+            img: img2,
+            name: 'Настя и Коля Какие-Нибудь',
+            date: 'comment1',
+            text: 'text1'
+        },
     ]
 
     const sliderSettings = {
@@ -33,8 +38,10 @@ const Reviews = () => {
         slidesToScroll: 1,
         className: 'custom-slider',
         nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />
+        prevArrow: <PrevArrow />,
+        dotsClass: 'dots',
     };
+
 
     function NextArrow(props) {
         const { onClick } = props;
@@ -56,6 +63,8 @@ const Reviews = () => {
         );
     }
 
+
+
     return (
         <section className={s.reviews}>
             <div className="container">
@@ -66,14 +75,13 @@ const Reviews = () => {
                     </div>
                     <div className="">
                         <Slider  {...sliderSettings}>
-                            <ReviewsItem />
-                            <ReviewsItem />
-                            <ReviewsItem />
+                            {reviews.map((reviews, index) =>
+                                <ReviewsItem img={reviews.img} name={reviews.name} date={reviews.date} text={reviews.text} />
+                            )}
                         </Slider>
                     </div>
                 </div>
             </div>
-
         </section>
     );
 };
--- src/components/reviews/reviewItem/ReviewsItem.jsx
+++ src/components/reviews/reviewItem/ReviewsItem.jsx
@@ -12,26 +12,21 @@ import s from "./ReviewsItem.module.scss";
 const ReviewsItem = (props) => {
 
     return (
-
-        <div className={s.item}>
+        <div className={s.item} >
             <div className={s.imgBlock}>
-                <img src="" alt="" />
+                <img src={props.img} alt="" />
             </div>
             <div className={s.textBlock}>
-                <div className={s.author}>{props.title}</div>
-                <div className={s.date}>{props.comment}</div>
-                <div className={s.text}>Всем привет!  Начинаются работы на стройке жилого комплекса.
-                    Предлагаю здесь обмениваться фотографиями и новостями. Всем привет!
-                    Начинаются работы на стройке жилого комплекса. Всем привет!
-                    Начинаются работы на стройке жилого комплекса. Предлагаю здесь обмениваться фотографиями и новостями.
+                <div className={s.author}>{props.name}</div>
+                <div className={s.date}>{props.date}</div>
+                <div className={s.text}>{props.text}
                 </div>
                 <div className={s.bottomBlock}></div>
             </div>
         </div>
-
-
     );
 };
 
 
 export default ReviewsItem;
+
