79be6bc5dcd00b2f8b1e23db17a2e54282e585fc
unknown <pakelvi0707@gmail.com>
2022-06-21 12:53:09 +0300

    quize

--- src/components/request/Rate.jsx
+++ src/components/request/Rate.jsx
@@ -9,37 +9,15 @@ import { Checkbox } from 'antd';
 
 
 
-
-
-const checkboxItem = [
-    {
-        title: "Прихожая",
-    },
-    {
-        title: "Гардеробная",
-    },
-    {
-        title: "Кухня",
-    },
-    {
-        title: "Гостиная",
-    },
-    {
-        title: "Спальня",
-    },
-    {
-        title: "Детская",
-    },
-    {
-        title: "Санузел",
-    },
-    {
-        title: "Кабинет",
-    }
-]
-
 const Rate = ({ nextStep }) => {
-    const [checked, setChecked] = React.useState(false);
+    const [checked1, setChecked1] = React.useState(false);
+    const [checked2, setChecked2] = React.useState(false);
+    const [checked3, setChecked3] = React.useState(false);
+    const [checked4, setChecked4] = React.useState(false);
+    const [checked5, setChecked5] = React.useState(false);
+    const [checked6, setChecked6] = React.useState(false);
+    const [checked7, setChecked7] = React.useState(false);
+    const [checked8, setChecked8] = React.useState(false);
 
 
     const data = {
@@ -68,26 +46,115 @@ const Rate = ({ nextStep }) => {
                         </div>
                         <div className={s.itemsQuize}>
                             <div className={s.checkboxes}>
-                                {checkboxItem.map((checkboxItem, index) =>
-                                    <div className="">
-                                        <Checkbox key={index}
-                                            id=''
-                                            value={checked} onChange={() => setChecked(!checked)} >
-                                            {checkboxItem.title}
-                                            
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked1} onChange={() => setChecked1(!checked1)}>
+                                            Прихожая
                                         </Checkbox>
-                                        <div className={s.selectItem}>
-                                            <div className="">
-                                                состояние: {checked ? 'отмечен' : 'не отмечен'}
-                                            </div>
-                                        </div>
                                     </div>
-                                )}
+                                    <div className={checked1 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked2} onChange={() => setChecked2(!checked2)}>
+                                            Гардеробная
+                                        </Checkbox>
+                                    </div>
+                                    <div className={checked2 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked3} onChange={() => setChecked3(!checked3)}>
+                                            Кухня
+                                        </Checkbox>
+                                    </div>
+                                    <div className={checked3 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked4} onChange={() => setChecked4(!checked4)}>
+                                            Гостиная
+                                        </Checkbox>
+                                    </div>
+                                    <div className={checked4 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked5} onChange={() => setChecked5(!checked5)}>
+                                            Спальня
+                                        </Checkbox>
+                                    </div>
+                                    <div className={checked5 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked6} onChange={() => setChecked6(!checked6)}>
+                                            Детская
+                                        </Checkbox>
+                                    </div>
+                                    <div className={checked6 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked7} onChange={() => setChecked7(!checked7)}>
+                                            Санузел
+                                        </Checkbox>
+                                    </div>
+                                    <div className={checked7 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={s.checkboxWrap}>
+                                    <div className={s.checkboxItem}>
+                                        <Checkbox value={checked8} onChange={() => setChecked8(!checked8)}>
+                                            Кабинет
+                                        </Checkbox>
+                                    </div>
+                                    <div className={checked8 ? 'description active' : 'description'}>
+                                        <button>
+                                            <img src="img/edit.svg" alt="" />
+                                            <span>добавить описание</span>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
 
                             <div className={s.userText}>
-                                <div className={s.title}></div>
-                                <textarea name="" id="" cols="30" rows="10"></textarea>
+                                <div className={s.title}>Произвольное пожелание</div>
+                                <textarea placeholder='Ваше пожелание...' name="" id="" cols="30" rows="10"></textarea>
                             </div>
                             <div className={s.next}>
                                 <div className={s.nextItem}>План БТИ</div>
