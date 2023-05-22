d854b856e784ddd1a598657652083d7ebc263bee
slava <doropheychik.viacheslav@gmail.com>
2023-05-22 17:17:32 +0300

    token expired fix

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,8 +1,14 @@
 import {useState} from 'react';
 import {hostName} from "../API/config";
+import {useNavigate} from "react-router";
+import {RouteNames} from "../router/routeNames";
+import {redActions} from "../reducer/actions";
+import {useDispatch} from "react-redux";
 
 export const useApi = (settings) => {
     const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+	const dispatch = useDispatch();
 
     const fetching = async () => {
         let resp = '';
@@ -21,10 +27,35 @@ export const useApi = (settings) => {
             resp = await response.json();
             if (!response.ok) {
                 setIsLoading(false);
-                if (resp.error.message) {
-					throw new Error(resp.error.message);
+				console.log(resp);
+				if (resp.error === "Unauthorized") {
+					console.log("Unauthorized");
+					localStorage.removeItem('token')
+					localStorage.removeItem('name')
+					localStorage.removeItem('surname')
+					localStorage.removeItem('email')
+					localStorage.removeItem('userId')
+					localStorage.removeItem('phoneNumber')
+					localStorage.removeItem('uuid')
+					localStorage.removeItem('jwtToken')
+					localStorage.removeItem('chatUserId')
+					localStorage.removeItem('exchangeName')
+					localStorage.removeItem('isAdmin')
+					dispatch({ type: redActions.setIsAuth, payload: false });
+					dispatch({ type: redActions.setIsAdmin, payload: false });
+					dispatch({ type: redActions.setUser, payload: {
+							name: '',
+							email: '',
+							phone: '',
+							id: ''
+						} });
+					navigate(RouteNames.LOGIN)
 				} else {
-					throw new Error(resp.error.description);
+					if (resp.error.message) {
+						throw new Error(resp.error.message);
+					} else {
+						throw new Error(resp.error.description);
+					}
 				}
             }
         } catch (e) {
